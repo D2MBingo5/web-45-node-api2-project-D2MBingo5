344e5a0bdec80138962af95016fdfef37a5fb1c7
Leu Family <romanianfobjr@gmail.com>
2021-08-31 15:35:34 -0500

    put command created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -45,4 +45,23 @@ router.post('/', (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        const possiblePost = await Post.findById(req.params.id)
+        if (!possiblePost) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        } else {
+            if (!req.body.title || !req.body.contents) {
+                res.status(400).json({ message: "Please provide title and contents for the post" })
+            } else {
+                const postUpdate = await Post.update(req.params.id, req.body)
+                res.status(200).json(postUpdate)
+            }
+        }
+    }
+    catch (err) {
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+
 module.exports = router
