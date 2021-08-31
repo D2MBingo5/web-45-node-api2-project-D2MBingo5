2219fa2493fbfa4577e95ce529014f82af0e8ab2
Leu Family <romanianfobjr@gmail.com>
2021-08-31 15:12:07 -0500

    get action 1 created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,15 @@ const express = require('express')
 const router = express.Router()
 const Post = require('./posts-model')
 
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.json(posts)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The posts information could not be retrieved" })
+        })
+})
+
 module.exports = router
