61c0edc6bd9bed0fac1080cdf7a5b3782cfd3c6c
Leu Family <romanianfobjr@gmail.com>
2021-08-31 15:55:13 -0500

    get comments command created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -79,4 +79,34 @@ router.delete('/:id', (req, res) => {
         })
 })
 
+router.get('/:id/comments', (req, res) => {
+    Post.findPostComments(req.params.id)
+        .then(comments => {
+            if (comments.length > 0) {
+                res.json(comments)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        })
+})
+
+// router.get('/:id', (req, res) => {
+//     Post.findById(req.params.id)
+//         .then(post => {
+//             if (post) {
+//                 res.json(post)
+//             } else {
+//                 res.status(404).json({ message: "The post with the specified ID does not exist" })                
+//             }
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json({ message: "The post information could not be retrieved" })
+//         })
+// })
+
 module.exports = router
